0bb3cb136524ab286d8a200fbf939b1d425f2188
Remi <dev.perrier.remi@gmail.com>
2020-03-10 11:04:01 +0100

    Retrieve link from michelin.fr

--- server/michelin.js
+++ server/michelin.js
@@ -11,7 +11,7 @@ const parse = data => {
   const name = $('.section-main h2.restaurant-details__heading--title').text();
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
 
-  return {name, experience};
+  return { name, experience };
 };
 
 /**
@@ -21,7 +21,7 @@ const parse = data => {
  */
 module.exports.scrapeRestaurant = async url => {
   const response = await axios(url);
-  const {data, status} = response;
+  const { data, status } = response;
 
   if (status >= 200 && status < 300) {
     return parse(data);
@@ -36,6 +36,28 @@ module.exports.scrapeRestaurant = async url => {
  * Get all France located Bib Gourmand restaurants
  * @return {Array} restaurants
  */
-module.exports.get = () => {
+module.exports.get = async () => {
+  var restaurants = [];
+  const url = "https://guide.michelin.com/fr/fr/restaurants/bib-gourmand";
+
+  try {
+    console.log("Fetch data");
+    const response = await axios(url);
+    const { data, status } = response;
+    console.log("Data fetched")
+
+    console.log("Load data");
+    const $ = cheerio.load(data);
+
+    $("div[class='col-md-6 col-lg-6 col-xl-3']").each(function (i, e) {
+      console.log($(this).find('a').attr('href'))
+    });
+
+    console.log("It's okay");
+
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
   return [];
 };
